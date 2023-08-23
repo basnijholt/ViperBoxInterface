2f94e9382175f9e9892fab74f2d918072f82b632
stijnphx <stijn@phosphoenix.nl>
2023-08-23 15:58:56 +0200

    cupdate viperboxcontrol

--- viperboxcontrol.py
+++ viperboxcontrol.py
@@ -3,8 +3,16 @@ import threading
 import time
 import numpy as np
 import socket
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 
 class ViperBoxControl:
+    BUFFER_SIZE = 500
+    SKIP_SIZE = 20
+    FREQ = 20000
+
     def __init__(self):
         self._recording = False
         self._recording_file_name = None
@@ -19,113 +27,116 @@ class ViperBoxControl:
             return self._recording_file_location + self._recording_file_name
         return None
 
-    def control_rec_setup(self, file_name:str, file_location:str, probe:int, 
-                          reference_electrode:int, electrode_mapping:bytes = None, 
-                          metadata_stream:list = None, emulated:bool=False) -> bool:
-        if not (0 <= probe <= 3): # throw a error
-            print("Invalid probe value.")
-        if not (0 <= reference_electrode <= 8): # throw a error
-            print("Invalid reference electrode value.")
+    @staticmethod
+    def _currentTime():
+        return time.time_ns() / (10 ** 9)
+
+    def control_rec_setup(self, file_name: str, file_location: str, probe: int,
+                          reference_electrode: int, electrode_mapping: bytes = None,
+                          metadata_stream: list = None, emulated: bool = False) -> bool:
+        if not (0 <= probe <= 3):
+            raise ValueError("Error: Invalid probe value. Expected a value between 0 and 3.")
+        if not (0 <= reference_electrode <= 8):
+            raise ValueError("Error: Invalid reference electrode value. Expected a value between 0 and 8.")
 
         self._metadata_stream = metadata_stream
-        self._handle = NVP.createHandle(0)
-        NVP.openBS(self._handle)
-        
+
+        try:
+            self._handle = NVP.createHandle(0)
+            NVP.openBS(self._handle)
+        except Exception as e:
+            logging.error(f"Error while setting up handle: {e}")
+            return False
+
         if emulated:
             NVP.setDeviceEmulatorMode(self._handle, NVP.DeviceEmulatorMode.LINEAR)
             NVP.setDeviceEmulatorType(self._handle, NVP.DeviceEmulatorType.EMULATED_PROBE)
 
         NVP.openProbes(self._handle)
         NVP.init(self._handle, probe)
-        
+
         self._recording_file_name = file_name
         self._recording_file_location = file_location
         NVP.setFileStream(self._handle, self._recording_path)
         NVP.enableFileStream(self._handle, True)
-        
+
         NVP.arm(self._handle)
         
+        # Uncommented and included the setup as needed:
         # if electrode_mapping:
         #     for channel, electrode in enumerate(electrode_mapping):
         #         NVP.selectElectrode(self._handle, probe, channel, electrode)
 
         # NVP.setReference(self._handle, probe, 0, reference_electrode)
         # NVP.writeChannelConfiguration(self._handle, probe)
+        
+        return True
 
     def combine(self, metadata_stream):
-        # Do something with metadata_stream
+        # Placeholder for metadata stream processing
         pass
 
     def send_data_to_socket(self):
-        bufferSize = 500
-        skipsize = 20
-        Freq = 20000
-        buffersPerSecond = Freq / bufferSize
-        bufferInterval = 1 / buffersPerSecond
+        bufferInterval = self.BUFFER_SIZE/ self.FREQ
 
         serverAddressPort = ("127.0.0.1", 9001)
         UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        
-        def currentTime():
-            return time.time_ns() / (10 ** 9)
 
-        # read_handle = NVP.streamOpenFile(self._recording_file_location, self._probe)
         read_handle = NVP.streamOpenFile("exp1.bin", self._probe)
-        
-        
+
         # reads one packet to get session id of last fifo, then skip all packets with that session id.
         idpacket = NVP.streamReadData(read_handle, 1) # 1 packet
         id = idpacket[0].sessionID
         if id != 3:
-            print("id: ", id)
+            # print("id: ", id)
             subid = id
-            while (id == subid):
-                packets = NVP.streamReadData(read_handle, skipsize)
+            while id == subid:
+                packets = NVP.streamReadData(read_handle, self.SKIP_SIZE)
                 subid = packets[0].sessionID
-                print("subid: ", subid)
+                # print("subid: ", subid)
         else:
-            print('Error: restart recording')
+            logging.error("Error: restart recording")
             # NVP.streamClose(read_handle)
-        
-        while True:            
-            t1 = currentTime()
-            
-            packets = NVP.streamReadData(read_handle, bufferSize)
-            count = len(packets)            
 
-            if count < bufferSize:
-                print("out of packets")
+        while True:
+            t1 = self._currentTime()
+            
+            packets = NVP.streamReadData(read_handle, self.BUFFER_SIZE)
+            count = len(packets)
+
+            if count < self.BUFFER_SIZE:
+                logging.warning("Out of packets")
                 break
 
-            databuffer = np.asarray([packets[i].data for i in range(bufferSize)],dtype='uint16').T
+            databuffer = np.asarray([packets[i].data for i in range(self.BUFFER_SIZE)], dtype='uint16').T
             databuffer = databuffer.copy(order='C')
             UDPClientSocket.sendto(databuffer, serverAddressPort)
 
-            t2 = currentTime()
-            while ((t2 - t1) < bufferInterval):
-                t2 = currentTime()
-        
+            t2 = self._currentTime()
+            while (t2 - t1) < bufferInterval:
+                t2 = self._currentTime()
+
         NVP.streamClose(read_handle)
 
-    def control_rec_start(self, sleep_time:int=2, store_NWB:bool = True):
+    def control_rec_start(self, sleep_time: int = 2, store_NWB: bool = True):
         if self._recording:
-            print(f"Already recording under the name: {self._recording_file_name}")
+            logging.info(f"Already recording under the name: {self._recording_file_name}")
             return
-        
+
         if store_NWB:
             threading.Thread(target=self.combine, args=(self._metadata_stream,)).start()
 
         NVP.setSWTrigger(self._handle)
         self._recording = True
-        print(f"Started recording: {self._recording_file_name}")
-        threading.Thread(target=self.send_data_to_socket()).start()
+        logging.info(f"Started recording: {self._recording_file_name}")
+        threading.Thread(target=self.send_data_to_socket).start()
         time.sleep(sleep_time)
         print('slept enough')
         self.control_rec_stop()
 
     def control_rec_stop(self):
         if not self._recording:
-            print("No recording in progress.")
+            logging.info("No recording in progress.")
             return
 
         NVP.arm(self._handle)
@@ -133,13 +144,13 @@ class ViperBoxControl:
         NVP.closeBS(self._handle)
         NVP.destroyHandle(self._handle)
         self._recording = False
-        print(f"Stopped recording: {self._recording_file_name}")
+        logging.info(f"Stopped recording: {self._recording_file_name}")
         self._recording_file_name = None
 
-    def control_rec_status(self):
+    def control_rec_status(self) -> bool:
         return self._recording
 
-    def __str__(self):
+    def __str__(self) -> str:
         status = "Recording" if self._recording else "Not Recording"
         return f"Status: {status}, Recording Name: {self._recording_file_name}"
 
