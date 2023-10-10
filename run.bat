b2ce437c9d562e65ef2a05562806c3bad77a1b0c
sbalk <stijn.balk@gmail.com>
2023-10-10 10:48:22 +0200

    added installation and run files

---
+++ run.bat
@@ -0,0 +1,3 @@
+@echo on
+call C:\ProgramData\anaconda3\Scripts\activate.bat C:\ProgramData\anaconda3\envs\neuraviper
+call C:\ProgramData\anaconda3\envs\neuraviper\python .\gui.py
---
+++ setup.bat
@@ -0,0 +1,3 @@
+@echo off
+PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& 'setup.ps1'"
+pause
