0dc203535658e670d4608f6b40e3d4702beb4706
sbalk <stijn.balk@gmail.com>
2024-01-23 10:53:37 +0100

    update VB_classes.py to python 3.12

--- VB_classes.py
+++ VB_classes.py
@@ -9,80 +9,80 @@ from typing import Dict
 
 @dataclass
 class GeneralSettings:
-    viperbox_software_id: str = None
-    session_starting_datetime: str = None
-    api_version: str = None
-    basestation_id: str = None
-    boot_code_version: str = None
-    head_stage_id: str = None
-    mezzanine_id_1: str = None
-    mezzanine_id_2: str = None
-    mezzanine_id_3: str = None
-    mezzanine_id_4: str = None
-    probe_id_1: str = None
-    probe_id_2: str = None
-    probe_id_3: str = None
-    probe_id_4: str = None
+    viperbox_software_id: str | None = None
+    session_starting_datetime: str | None = None
+    api_version: str | None = None
+    basestation_id: str | None = None
+    boot_code_version: str | None = None
+    head_stage_id: str | None = None
+    mezzanine_id_1: str | None = None
+    mezzanine_id_2: str | None = None
+    mezzanine_id_3: str | None = None
+    mezzanine_id_4: str | None = None
+    probe_id_1: str | None = None
+    probe_id_2: str | None = None
+    probe_id_3: str | None = None
+    probe_id_4: str | None = None
 
 
 @dataclass
 class StimulationUnitSettings:
-    stimulation_unit: int = None
-    polarity: bool = None
-    pulses: int = None
-    prephase: int = None
-    amplitude1: int = None
-    width1: int = None
-    interphase: int = None
-    amplitude2: int = None
-    width2: int = None
-    discharge: int = None
-    duration: int = None
-    aftertrain: int = None
+    stimulation_unit: int | None = None
+    polarity: bool | None = None
+    pulses: int | None = None
+    prephase: int | None = None
+    amplitude1: int | None = None
+    width1: int | None = None
+    interphase: int | None = None
+    amplitude2: int | None = None
+    width2: int | None = None
+    discharge: int | None = None
+    duration: int | None = None
+    aftertrain: int | None = None
 
 
 @dataclass
 class ElectrodeSet:
-    stimulation_unit: int = None
-    electrode_set: list = None
+    stimulation_unit: int | None = None
+    electrode_set: list | None = None
 
 
 @dataclass
 class ChannelSettings:
-    channel: int = None
-    body_reference: bool = None
-    shank_references: str = None
-    gain: int = None
-    input: int = None
+    channel: int | None = None
+    body_reference: bool | None = None
+    shank_references: str | None = None
+    gain: int | None = None
+    input: int | None = None
 
 
 @dataclass
 class ProbeRecordingSettings:
     # The int in the Dict is the channel number
-    probe: int = None
-    channel_settings: Dict[int, ChannelSettings] = [None]
+    probe: int | None = None
+    channel_settings: Dict[int, ChannelSettings] | None = None
 
 
 @dataclass
 class ProbeStimulationSettings:
     # The int in the Dict is the stimulation unit number
-    probe: int = None
-    stimulation_unit_settings: Dict[int, StimulationUnitSettings] = [None]
-    stimulation_unit_electrodes: Dict[int, ElectrodeSet] = [None]
+    probe: int | None = None
+    stimulation_unit_settings: Dict[int, StimulationUnitSettings] | None = None
+    stimulation_unit_electrodes: Dict[int, ElectrodeSet] | None = None
 
 
 @dataclass
 class HandleSettings:
     # The int in the Dict is the probe number
-    general_settings: GeneralSettings = None
-    handle: str = None
-    probes_recording: Dict[int, ProbeRecordingSettings] = [None]
-    probes_stimulation: Dict[int, ProbeStimulationSettings] = [None]
+    general_settings: GeneralSettings | None = None
+    handle: str | None = None
+    probes_recording: Dict[int, ProbeRecordingSettings] | None = None
+    probes_stimulation: Dict[int, ProbeStimulationSettings] | None = None
 
 
 @dataclass
 class TTLSettings:
-    TTL_channel: int = None
-    probes: int = None
-    stimulation_unit_settings: StimulationUnitSettings = None
-    electrode_selection: ElectrodeSet = None
+    TTL_channel: int | None = None
+    probes: int | None = None
+    stimulation_unit_settings: StimulationUnitSettings | None = None
+    electrode_selection: ElectrodeSet | None = None
