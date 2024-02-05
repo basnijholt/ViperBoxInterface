790d67e78870da3249551bb1421e1dddc2b66eff
sbalk <stijn.balk@gmail.com>
2024-02-05 21:54:30 +0100

    update vb_classes

--- VB_classes.py
+++ VB_classes.py
@@ -3,11 +3,14 @@ from dataclasses import asdict, dataclass, field, is_dataclass
 from typing import Any, Dict, List
 
 import numpy as np
+from lxml import etree
 
 # handle contains 4 probes
 # each probe contains 64 channels, 8 SU's and 128 electrodes
 # recording goes through the 64 channels
 # stimulation goes through the 8 SU's
+
+# Stack overflow nested data classes https://stackoverflow.com/questions/51564841/creating-nested-dataclass-objects-in-python
 
 
 @dataclass
@@ -34,7 +37,7 @@ class SUSettings:
     pulses: int = 0
     prephase: int = 0
     amplitude1: int = 0
-    width1: int = 0
+    width1: int = 170
     interphase: int = 0
     amplitude2: int = 0
     width2: int = 0
@@ -145,7 +148,7 @@ class TTL_handels:
 
 @dataclass
 class HandleSettings:
-    handle: str = ""
+    handle_id: str = ""
     hardware_id_base_station: IDInformation | None = None
     hardware_id_head_stage: IDInformation | None = None
     probes: Dict[int, ProbeSettings] = field(default_factory=dict)
@@ -159,6 +162,19 @@ class GeneralSettings:
     api_version_major: str = ""
     # handle_sett: HandleSettings = field(default_factory=HandleSettings)
     handles: Dict[int, HandleSettings] = field(default_factory=dict)
+    # connected: Dict[int, List[int]] = field(default_factory=dict)
+
+    @property
+    def connected(self):
+        connected_hp = {}
+        if self.handles:
+            handle_list = list(self.handles.keys())
+            for handle in handle_list:
+                if self.handles[handle].probes:
+                    connected_hp[handle] = list(self.handles[handle].probes.keys())
+            return connected_hp
+        else:
+            return connected_hp
 
 
 def printable_dtd(obj: Any) -> None:
@@ -167,6 +183,21 @@ def printable_dtd(obj: Any) -> None:
     """
 
     print(json.dumps(dataclass_to_dict(obj), indent=4, sort_keys=True))
+
+
+def readable_dtd(obj: Any) -> None:
+    """
+    Recursively convert dataclass instances to dictionaries.
+    """
+
+    return json.dumps(dataclass_to_dict(obj), indent=4, sort_keys=True)
+
+
+def printet(obj: Any) -> None:
+    """
+    Readable print of an ET object.
+    """
+    print(etree.tostring(obj, pretty_print=True).decode())
 
 
 def dataclass_to_dict(obj: Any) -> Any:
@@ -204,3 +235,112 @@ def dict_to_dataclass(cls: Any, dict_obj: Any) -> Any:
         )
     else:
         return dict_obj
+
+
+def parse_numbers(numstr: str, all_values: list[int]):
+    """Parse a string of numbers into a numpy array of integers. These are inputs like:
+    '1,2,3,4-6,8' or '-' for all possible values.
+
+    Arguments:
+    - numstr {str} -- string of numbers to parse
+    - all_values {list[int]} -- list of all possible values for the numbers
+
+    Returns:
+    - result {numpy.ndarray} -- numpy array of parsed integers
+
+    Test cases:
+    - wrong ranges: '1-2-3', '1-', '1,,2', '1--2', '-1'
+    might be more.
+    """
+
+    if ",," in numstr:
+        raise ValueError("Invalid input, can't put double commas")
+    if "--" in numstr:
+        raise ValueError("Invalid input, can't put double dashes")
+    result = np.array([], dtype=int)
+    if numstr == "-":
+        if all_values is None:
+            raise ValueError("all_values not known")
+        result = np.asarray(all_values)
+    elif len(numstr) == 1:
+        result = np.array([int(numstr)])
+    else:
+        split_numstr = numstr.split(",")
+        for item in split_numstr:
+            if item[0] == "-":
+                raise ValueError("Invalid input, can't start with a dash")
+            if item[-1] == "-":
+                raise ValueError("Invalid input, can't end with a dash")
+            if "-" in item:
+                split_item = item.split("-")
+                # check if the range is valid
+                if len(split_item) != 2:
+                    raise ValueError("Invalid range")
+                elif int(split_item[0]) > int(split_item[1]):
+                    np_item = np.asarray(
+                        range(int(split_item[1]), int(split_item[0]) + 1)
+                    )
+                else:
+                    np_item = np.asarray(
+                        range(int(split_item[0]), int(split_item[1]) + 1)
+                    )
+            else:
+                np_item = np.asarray(int(item))
+            result = np.append(result, np_item)
+        result = np.unique(result)
+    if not set(result).issubset(set(all_values)):
+        raise ValueError(
+            "Invalid values; following are not connected"
+            ":{set(result) - set(all_values)}."
+        )
+    return result.tolist()
+
+
+def get_handles(settings):
+    """Get the handles of all settings in the settings dictionary
+
+    Arguments:
+    - settings {dict} -- dictionary of settings
+
+    Returns:
+    - handles {list} -- list of handles
+    """
+    handles = [int(i) for i in settings["handles"].keys()]
+    if not handles:
+        raise ValueError("No handles found in settings")
+    return handles
+
+
+def get_probes(handle: int, settings):
+    """Get the probes of a handle
+
+    Arguments:
+    - handle {int} -- handle of the setting
+    - settings {dict} -- dictionary of settings
+
+    Returns:
+    - probes {list} -- list of probes
+    """
+    probes = [int(i) for i in settings["handles"][str(handle)]["probes"].keys()]
+    if not probes:
+        raise ValueError(f"No probes found in handle {handle}")
+    return probes
+
+
+def __check_handles_exist(data, existing_handles):
+    """Check if xml handles are in existing handles. If not, throw ValueError, else pass
+
+    Arguments:
+    - data: xml data of type lxml.etree._ElementTree
+    - existing_handles: list of existing handles
+    TODO: existing handles should be changed to something that comes from the local
+    settings.
+
+    test cases:
+    - xml handle is not in existing handles
+
+    """
+    for element in data.xpath(".//*[@handle]"):
+        setting_handles = element.attrib["handle"]
+        _ = parse_numbers(setting_handles, existing_handles)
+    return True
