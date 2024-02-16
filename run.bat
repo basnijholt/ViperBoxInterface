927102291c0a0b8d0362500a9b6d416c5eab7c81
sbalk <stijn.balk@gmail.com>
2024-02-16 10:38:04 +0100

    cleanup dev branch

--- run.bat
+++
@@ -1,3 +0,0 @@
-@echo on
-call C:\ProgramData\anaconda3\Scripts\activate.bat C:\ProgramData\anaconda3\envs\neuraviper
-call C:\ProgramData\anaconda3\envs\neuraviper\python .\gui.py
--- update.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-call git fetch --all
-call git reset --hard origin/dev
